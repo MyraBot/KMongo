2c04df4796c3d677d1d6668ed1f006a35c838ce9
Marian <elias-marian@web.de>
2021-12-18 16:51:22 +0100

    :boom: Added leaderboards

--- src/main/kotlin/com/github/myra/kmongo/data/guild/DbGuild.kt
+++ src/main/kotlin/com/github/myra/kmongo/data/guild/DbGuild.kt
@@ -3,9 +3,15 @@ package com.github.myra.kmongo.data.guild
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.github.m5rian.kotlingua.Kotlingua
 import com.github.m5rian.kotlingua.Lang
+import com.github.myra.kmongo.Mongo
 import com.github.myra.kmongo.cache.impl.guild.*
+import com.github.myra.kmongo.data.member.DbMember
+import com.github.myraBot.diskord.Diskord
 import com.github.myraBot.diskord.common.entities.guild.Guild
+import com.github.myraBot.diskord.common.entities.guild.Member
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
+import org.litote.kmongo.eq
 import org.litote.kmongo.pull
 import org.litote.kmongo.push
 
@@ -37,3 +43,40 @@ suspend fun Guild.suggestions(): DbSuggestions = CacheGuildSuggestions.load(this
 suspend fun Guild.welcoming(): DbWelcoming = CacheGuildWelcoming.load(this.id)
 suspend fun Guild.youtube(): DbYoutube = CacheGuildNotificationsYoutube.load(this.id)
 suspend fun Guild.twitch(): DbTwitch = CacheGuildNotificationsTwitch.load(this.id)
+
+val Guild.levelLeaderboard: List<Member>
+    get() {
+        val members = Mongo.getAs<DbMember>("members")
+            .find(DbMember::guildId eq this.id)
+            .toMutableList()
+        members.sortWith(Comparator.comparing(DbMember::level).reversed())
+        val guild = Diskord.getGuild(this.id)
+        return members.mapNotNull { runBlocking { guild?.getMember(it.userId) } }
+    }
+val Guild.voiceTimeLeaderboard: List<Member>
+    get() {
+        val members = Mongo.getAs<DbMember>("members")
+            .find(DbMember::guildId eq this.id)
+            .toMutableList()
+        members.sortWith(Comparator.comparing(DbMember::voiceCallTime).reversed())
+        val guild = Diskord.getGuild(this.id)
+        return members.mapNotNull { runBlocking { guild?.getMember(it.userId) } }
+    }
+val Guild.balanceLeaderboard: List<Member>
+    get() {
+        val members = Mongo.getAs<DbMember>("members")
+            .find(DbMember::guildId eq this.id)
+            .toMutableList()
+        members.sortWith(Comparator.comparing(DbMember::balance).reversed())
+        val guild = Diskord.getGuild(this.id)
+        return members.mapNotNull { runBlocking { guild?.getMember(it.userId) } }
+    }
+val Guild.dailyStreakLeaderboard: List<Member>
+    get() {
+        val members = Mongo.getAs<DbMember>("members")
+            .find(DbMember::guildId eq this.id)
+            .toMutableList()
+        members.sortWith(Comparator.comparing(DbMember::dailyStreak).reversed())
+        val guild = Diskord.getGuild(this.id)
+        return members.mapNotNull { runBlocking { guild?.getMember(it.userId) } }
+    }
