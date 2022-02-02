ae97aa095eeca247b13c29c2e23cd7be90abec7a
Marian <elias-marian@web.de>
2022-02-02 16:35:01 +0100

    :art: Format code

--- src/main/kotlin/com/github/myra/kmongo/data/guild/DbWelcoming.kt
+++ src/main/kotlin/com/github/myra/kmongo/data/guild/DbWelcoming.kt
@@ -6,29 +6,29 @@ import java.awt.Color
 
 @Serializable
 data class DbWelcoming(
-        val guildId: String,
-        val channel: String?,
-        val directMessage: DbDirectMessage,
-        val embed: DbEmbed,
-        val image: DbImage
+    val guildId: String,
+    val channel: String?,
+    val directMessage: DbDirectMessage,
+    val embed: DbEmbed,
+    val image: DbImage
 )
 
 @Serializable
 data class DbDirectMessage(
-        val toggled: Boolean,
-        val message: String?
+    val toggled: Boolean,
+    val message: String?
 )
 
 @Serializable
 data class DbEmbed(
-        val toggled: Boolean,
-        val message: String?,
-       @Serializable(with = ColorSerializer::class) val colour: Color?
+    val toggled: Boolean,
+    val message: String?,
+    @Serializable(with = ColorSerializer::class) val colour: Color?
 )
 
 @Serializable
 data class DbImage(
-        val toggled: Boolean,
-        val image: String?,
-        val font: String
+    val toggled: Boolean,
+    val image: String?,
+    val font: String
 )
