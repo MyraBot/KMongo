39771e98c4b9e967aa6122d290afd229b2ed0570
Marian <elias-marian@web.de>
2022-02-04 15:38:40 +0100

    :bug: Registered custom serializer

--- src/main/kotlin/com/github/myra/kmongo/Mongo.kt
+++ src/main/kotlin/com/github/myra/kmongo/Mongo.kt
@@ -2,10 +2,15 @@ package com.github.myra.kmongo
 
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.internal.MongoDatabaseImpl
+import de.undercouch.bson4jackson.serializers.BsonSerializers
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.serialization.serializer
 import org.bson.Document
+import org.bson.conversions.Bson
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.getCollection
+import org.litote.kmongo.serialization.registerSerializer
 
 object Mongo {
     lateinit var connectionString: String
@@ -17,6 +22,7 @@ object Mongo {
 
     fun connect() {
         mongo = KMongo.createClient(connectionString).getDatabase(database)
+        registerSerializer(ColorHexSerializer)
     }
 
     fun get(collection: String): MongoCollection<Document> {
