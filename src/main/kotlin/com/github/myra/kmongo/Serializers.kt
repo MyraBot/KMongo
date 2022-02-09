d71c51645ec4599aad29c9a6c28715db914e0340
Marian <elias-marian@web.de>
2022-02-09 17:13:11 +0100

    :bug: Fixed serializer *pls kill me*

--- src/main/kotlin/com/github/myra/kmongo/Serializers.kt
+++ src/main/kotlin/com/github/myra/kmongo/Serializers.kt
@@ -1,7 +1,6 @@
 package com.github.myra.kmongo
 
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
@@ -9,22 +8,15 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.awt.Color
 
-@Serializer(forClass = Color::class)
-internal object ColorHexSerializer : KSerializer<Color?> {
+internal object ColorHexSerializer : KSerializer<Color> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("color_hex", PrimitiveKind.STRING)
 
-    override fun serialize(encoder: Encoder, value: Color?) {
-        if (value == null) {
-            encoder.encodeNull()
-        } else {
-            val hex = String.format("#%06x", value.rgb and 0xFFFFFF)
-            encoder.encodeString(hex)
-        }
+    override fun serialize(encoder: Encoder, value: Color) {
+        val hex = String.format("#%06x", value.rgb and 0xFFFFFF)
+        encoder.encodeString(hex)
     }
 
-    override fun deserialize(decoder: Decoder): Color? {
-        //val hex = decoder.decodeString()
-        //return Color.decode(hex)r
-        return Color.BLUE
+    override fun deserialize(decoder: Decoder): Color {
+        return Color.decode(decoder.decodeString())
     }
 }
